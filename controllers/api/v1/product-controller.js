0b4510b2fa83d04c2ba39a412dc853320abc0e4b
Nishant Mishra <nishdrummer@gmail.com>
2021-09-17 14:20:40 +0530

    added update api

--- controllers/api/v1/product-controller.js
+++ controllers/api/v1/product-controller.js
@@ -1,12 +1,16 @@
 const ProductModal = require("../../../modal/Product");
-const { validationResult } = require('express-validator');
+const {
+    validationResult
+} = require('express-validator');
 
 module.exports.createProduct = async (req, res) => {
     try {
-        const errors= validationResult(req);
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-          }
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
         // const data = req.body || {};
         const data = await ProductModal.create(req.body);
         res.status(200).json({
@@ -36,35 +40,9 @@ module.exports.getProduct = async (req, res) => {
     }
 }
 
-
-module.exports.updateProduct = async (req, res) => {
-    try {
-        console.log(req.name);
-        const data = await ProductModal.updateOne({}
-            // {name:"Nishant"}, 
-            // function (err, docs) {
-            //     if (err){
-            //         console.log(err)
-            //     }
-            //     else{
-            //         console.log("Updated Docs : ", docs);
-            //     }}
-            );
-        res.status(200).json({
-            msg: "Products",
-            data: data
-        });
-    } catch (error) {
-        console.log("Error in updating Product", error);
-        res.status(500).json({
-            msg: "Internal Server Error"
-        });
-    }
-};
-
 module.exports.deleteProduct = async (req, res) => {
     try {
-        const data = await ProductModal.deleteONE({});
+        const data = await ProductModal.deleteOne({});
         res.status(200).json({
             msg: "Products",
             data: data
@@ -76,3 +54,60 @@ module.exports.deleteProduct = async (req, res) => {
         });
     }
 };
+
+
+module.exports.updateProduct = async (req, res) => {
+    try {
+        const data = await ProductModal.findByIdAndUpdate(
+            req.params["id"],
+            req.body, {
+                new: true
+            }
+        );
+
+        if (!data) {
+            res
+                .status(400)
+                .json({
+                    msg: "NOT FOUND",
+                    data: "Item with id not found."
+                });
+        } else {
+            res.status(200).json({
+                msg: "UPDATE SUCCESS",
+                data: data
+            });
+        }
+    } catch (error) {
+        console.error("Error in updating product", error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        });
+    }
+};
+
+
+// module.exports.updateProduct = async (req, res) => {
+//     try {
+//         console.log(req.name);
+//         const data = await ProductModal.updateOne({}
+//             // {name:"Nishant"}, 
+//             // function (err, docs) {
+//             //     if (err){
+//             //         console.log(err)
+//             //     }
+//             //     else{
+//             //         console.log("Updated Docs : ", docs);
+//             //     }}
+//             );
+//         res.status(200).json({
+//             msg: "Products",
+//             data: data
+//         });
+//     } catch (error) {
+//         console.log("Error in updating Product", error);
+//         res.status(500).json({
+//             msg: "Internal Server Error"
+//         });
+//     }
+// };
--- routes/api/v1/product.js
+++ routes/api/v1/product.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const {
+    body,
+    checkSchema
+} = require('express-validator');
 const productController = require('../../../controllers/api/v1/product-controller');
 
 router.post("/create", [
     body("name").not().isEmpty().withMessage("Name is Empty"),
-    body("quantity").isFloat({gt:0}).withMessage("Quantity is less than 0"),
+    body("quantity").isFloat({
+        gt: 0
+    }).withMessage("Quantity is less than 0"),
     body("description").not().isEmpty().withMessage("Description is Empty"),
-    body("price").isFloat({gt:0}).withMessage("Price is less than 0")
+    body("price").isFloat({
+        gt: 0
+    }).withMessage("Price is less than 0")
 
-],productController.createProduct);
+], productController.createProduct);
+
 router.get("/all", productController.getProduct);
-router.put("/update", productController.updateProduct);
+
+router.patch("/update/:id", checkSchema({
+    id: {
+        in: ["params"],
+        errorMessage: "ID is wrong",
+        isMongoId: true,
+    }
+}), productController.updateProduct);
+
 router.delete("/delete", productController.deleteProduct);
 module.exports = router;
